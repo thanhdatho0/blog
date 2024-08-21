67b93458339668d03d8ef684070520dbbad83947
Dat <thanhdatho0@gmail.com>
2024-08-21 13:21:54 +0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//Morgan
+app.use(morgan('combined'));
+
+//Template Engine (Handlebars)
+app.engine('hbs', handlebars.engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
